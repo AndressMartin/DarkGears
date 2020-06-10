3ef6b8d3518e7e1513a6779858f52f9edb386d17
GuilhermeSouzaVieira <guilherme123-40@hotmail.com>
2020-06-10 20:08:03 -0300

    Código de listagem de personagens e equipamento

---
+++ listaEncadeadaPersonagem.h
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+typedef struct{
+	char descricao[10];
+	int tipo_item;
+	int atk;
+	int def;
+	int hit;
+}DetalhesItem;
+
+typedef struct item{
+    DetalhesItem detalhes_item;
+    struct item *prox;
+    struct item *ant;
+}Item; //definição da lista
+
+typedef struct personagem{
+	int id;
+	char nome[10];
+	int atk;
+	int def;
+	int hit;
+	Item *itens = NULL;
+    struct personagem *ant; 
+    struct personagem *prox;
+}Personagens; //definição da lista
+
+Personagens * lista_cria()
+{
+    return NULL;
+}
+
+Item * lista_itens_cria()
+{
+    return NULL;
+}
+
+/* insere o novo dado no início da lista  e retorna a lista atualizada*/
+Personagens* lista_insere( Personagens* li, int id, char nome[], int atk, int def, int hit)
+{
+  	Personagens *novo= (Personagens*) malloc (sizeof(Personagens));
+  	strcpy(novo->nome, nome);
+    novo->atk = atk;
+    novo->def = def;
+    novo->hit = hit;
+    novo->prox=li;
+    novo->ant=NULL;
+    
+    //verifica se a lista não esta vazia
+    if(li!= NULL)
+        li->ant=novo;         
+    return novo;
+}
+
+/* retorna 1 se vazia ou zero se falso*/
+int lista_vazia(Personagens* li)
+{     
+    return(li==NULL); 
+}         
+
+int lista_itens_vazia(Item* li)
+{     
+    return(li==NULL); 
+}  
+
+/* retorna o elemento ou NULL se não achou*/
+Personagens* lista_busca(Personagens* li, int id)
+{     
+	//variável usada para percorrer a lista
+	Personagens *a; 
+	for(a=li;a!=NULL;a=a->prox)
+	{ 
+	    if(a->id == id) return a;
+	}         
+	return NULL;
+}
+
+Item* lista_itens_busca(Item* li, int tipo_item)
+{     
+	//variável usada para percorrer a lista
+	Item *a; 
+	for(a=li;a!=NULL;a=a->prox)
+	{ 
+	    if(a->detalhes_item.tipo_item == tipo_item) return a;
+	}         
+	return NULL;
+}
+
+/* retorna o novo inicio ou a lista original*/
+Personagens* lista_retira(Personagens* li, int tipo_item){
+ 	//procura o elemento na lista usando a função busca
+    Personagens* aux=lista_busca(li, tipo_item);
+    if (aux==NULL) //não achou o elemento
+        return li; 
+    //retira o elemento encadeado
+    if(li==aux) //verifica se é o primeiro da lista
+    	li=aux->prox;
+    else
+        aux->ant->prox=aux->prox;
+    //testa para ver se é o ultimo da lista
+    if (aux->prox!=NULL)        
+        aux->prox->ant=aux->ant;
+    free(aux);
+    return li;
+}
+
+Item* lista_itens_retira(Item* li, int id){
+ 	//procura o elemento na lista usando a função busca
+    Item* aux=lista_itens_busca(li, id);
+    if (aux==NULL) //não achou o elemento
+        return li; 
+    //retira o elemento encadeado
+    if(li==aux) //verifica se é o primeiro da lista
+    	li=aux->prox;
+    else
+        aux->ant->prox=aux->prox;
+    //testa para ver se é o ultimo da lista
+    if (aux->prox!=NULL)        
+        aux->prox->ant=aux->ant;
+    free(aux);
+    return li;
+}
+
+Item* lista_itens_insere(Item* li, DetalhesItem detalhes)
+{
+	// Procura e remove da lista caso já houver um item do mesmo tipo equipado;
+	li = lista_itens_retira(li, detalhes.tipo_item);
+	
+  	Item *novo= (Item*) malloc (sizeof(Item));
+    strcpy(novo->detalhes_item.descricao, detalhes.descricao);
+    novo->detalhes_item.atk = detalhes.atk;
+    novo->detalhes_item.def = detalhes.def;
+    novo->detalhes_item.hit = detalhes.hit;
+    novo->detalhes_item.tipo_item = detalhes.tipo_item;
+    novo->prox=li;
+    novo->ant=NULL;
+    
+    if(li!= NULL)
+        li->ant=novo;         
+    return novo;
+}
+
+Personagens* equipa_item(Personagens*li, int id, DetalhesItem detalhes)
+{
+	Personagens* personagem = lista_busca(li, id);
+	
+	if(personagem != NULL)
+	{
+		personagem->itens = lista_itens_insere(personagem->itens, detalhes);
+	}
+	
+	return li;
+}
