c368fb7bb22178c9da86903c425524d8195f293d
AM <42077892+AndressMartin@users.noreply.github.com>
2020-06-24 21:16:02 -0300

    Alterando o texto com "#"
    Kelvin, lembrando que tem que adicionar o argumento no ReadDialogue() para ler o char txtChanger[40].

--- caixaDeDialogo.h
+++ caixaDeDialogo.h
@@ -1,6 +1,6 @@
 #ifndef CAIXADEDIALOGO_H
 #define CAIXADEDIALOGO_H
 
-void ReadDialogue(int posx, int posy, int partToStart, int partToStop, char *Arquivo, void* Sprites_Retratos[], void* Sprites_Retratos_Mascaras[], void* Sprites_HUD[], void* Sprites_HUD_Mascaras[]);
+void ReadDialogue(int posx, int posy, int partToStart, int partToStop, char txtChanger[40], char *Arquivo, void* Sprites_Retratos[], void* Sprites_Retratos_Mascaras[], void* Sprites_HUD[], void* Sprites_HUD_Mascaras[]);
 
 #endif
